cf041da33f3e56e722023a15aa3951dcd335e564
kaulfield23 <kaulfield23@gmail.com>
2022-11-03 21:37:36 +0100

    added register user field

--- fullstack-front/src/users/AddUser.tsx
+++ fullstack-front/src/users/AddUser.tsx
@@ -1,7 +1,54 @@
 import React from "react";
 
 const AddUser = () => {
-  return <div>Add user</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">Register User</h2>
+          <div className="mb-3">
+            <label htmlFor="Name" className="form-label">
+              Name
+            </label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter your name"
+              name="name"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="UserName" className="form-label">
+              UserName
+            </label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter your username"
+              name="Username"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="Email" className="form-label">
+              Email
+            </label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter your Email address"
+              name="Email"
+            />
+          </div>
+          <button type="submit" className="btn btn-outline-primary">
+            Submit
+          </button>
+          <button type="submit" className="btn btn-outline-danger mx-2">
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddUser;
